18b0ac9cb95d9c176d737e66c28f5a1cb2cef410
epapad04 <32490686+epapad04@users.noreply.github.com>
2017-11-20 20:06:58 +0200

    Update DB_creation_script.sql

--- SQL/DB_creation_script.sql
+++ SQL/DB_creation_script.sql
@@ -1,1 +1,43 @@
 /*Here will put all thesql creation script after export from php my admin*/
+CREATE DATABASE IF NOT EXISTS qnaDB;
+CREATE TABLE IF NOT EXISTS Professor(
+    username varchar(10),
+    password varchar(10),
+    email	varchar(30),
+    PRIMARY KEY(username)
+);
+
+
+CREATE TABLE IF NOT EXISTS Course(
+    cname varchar(10),
+    username varchar(10),
+    dateof date,
+    PRIMARY KEY(cname,dateof),
+    CONSTRAINT FOREIGN KEY (username) REFERENCES Professor(username)
+);
+
+CREATE TABLE IF NOT EXISTS Lecture(
+    lid int,
+    cname varchar(10),
+    PRIMARY KEY(lid),
+    FOREIGN KEY (cname) REFERENCES Course(cname)
+);
+
+CREATE TABLE IF NOT EXISTS Question(
+    qid int,
+    lid int,
+    seen int,
+    textofquestion text,
+    textofanswer	text,
+    PRIMARY KEY(qid),
+    FOREIGN KEY (lid) REFERENCES Lecture(lid)
+);
+
+CREATE TABLE IF NOT EXISTS Answer(
+    aid int,
+    qid int,
+    textofanswer text,
+    counter int,
+    PRIMARY KEY(aid),
+    FOREIGN KEY (qid) REFERENCES Question(qid)
+);
