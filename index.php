a204928ccbc1a95cd5393b29050cc33cd4be5d37
ssavva05 <ssavva05@ucy.ac.cy>
2017-11-20 11:17:33 +0200

    configure bootstrap

--- index.php
+++ index.php
@@ -6,12 +6,34 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="UTF-8">       
+        
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
+       
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="./js/bootstrap.js"></script>
+        <script src="./js/npm.js"></script>
+        <title>Q&amp;A</title>
     </head>
     <body>
         <?php
         // put your code here
+        alert("ok");
         ?>
+        <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" type="email">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input id="inputPassword" class="form-control" placeholder="Password" required="" type="password">
+        <div class="checkbox">
+          <label>
+            <input value="remember-me" type="checkbox"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+        
     </body>
 </html>
