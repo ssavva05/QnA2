8f3bd66c3edfa6f58328b94ebbdd43d74bf9442c
ssavva05 <ssavva05@ucy.ac.cy>
2017-11-23 17:39:46 +0200

    update enroll view

--- enroll/enroll/main_enroll.php
+++ enroll/enroll/main_enroll.php
@@ -16,23 +16,23 @@ if (isset($_SESSION['enroll_key'])) {
     </head>
 
     <body>
-            <form class="form-signin" name="form1" method="post" action="checkenroll.php">
-                <img src="images/logo_en.png" alt="Univercity of Cyprus Logo" width="99%">
-                <img src="images/header.png" alt="Please Enroll to Lecture" width="100%">
-                <!--<h2 class="form-signin-heading">Please Enroll to Lecture</h2>-->
-                <input name="myenrollkey" id="myenrollkey" type="password" class="form-control" placeholder="Enrollment key" autofocus>
-                <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Enroll</button>
-                <div id="message"></div>
-            </form>
+        <form class="form-signin" name="form1" method="post" action="checkenroll.php">
+            <img src="images/logo_en.png" alt="Univercity of Cyprus Logo" width="99%">
+            <img src="images/header.png" alt="Please Enroll to Lecture" width="100%">
+            <!--<h2 class="form-signin-heading">Please Enroll to Lecture</h2>-->
+            <input name="myenrollkey" id="myenrollkey" type="password" class="form-control" placeholder="Enrollment key" autofocus>
+            <button name="Submit" id="submit" class="btn btn-lg btn-primary btn-block" type="submit">Enroll</button>
+            <div id="message"></div>
+        </form>
 
-        </div> <!-- /container -->
+    </div> <!-- /container -->
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="js/jquery-2.2.4.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script type="text/javascript" src="js/bootstrap.js"></script>
-        <!-- The AJAX login script -->
-        <script src="js/enroll.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <!-- The AJAX login script -->
+    <script src="js/enroll.js"></script>
 
-    </body>
+</body>
 </html>
