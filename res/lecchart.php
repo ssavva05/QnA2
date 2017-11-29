bbc05b2e7bffb1394dc7d360f85c0e357c5e6e40
ssavva05 <ssavva05@ucy.ac.cy>
2017-11-30 01:22:42 +0200

    core update

---
+++ res/lecchart.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+
+//if (!isset($_SESSION['username'])) {
+//    return header("location:../index.php");
+//}
+
+
+$temptab = filter_input_array(INPUT_POST);
+if (empty($temptab)) {
+    return header("location:../index.php");
+} else {
+
+
+
+//setting header to json
+    //header('Content-Type: application/json');
+    //$tbl_quest = "question";
+    //$tbl_ans = "answer";
+//print_r($temptab);
+
+    foreach ($temptab as $key => $value) {
+        $lid = $value;
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chart</title>
+        <style type="text/css">
+            #chart-container {
+                width: auto;
+                height: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="chart-container">
+            <canvas id="mycanvas"></canvas>
+        </div>
+
+        <!-- javascript -->
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/Chart.min.js"></script>
+        <!--<script type="text/javascript" src="js/app.js"></script>-->
+
+        <script>
+
+            function getRandomColor() {
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
+
+            $(document).ready(function () {
+                $.ajax({
+                    url: "http://localhost/QnA/res/lecdata.php",
+                    method: "POST",
+<?= "data : {" . $lid . " : " . $lid . "}," ?>
+
+                    success: function (data) {
+                        //console.log(data);
+                        var player = [];
+                        var score = [];
+                        var color = [];
+
+
+                        for (var i in data) {
+                            player.push("Answer:" + data[i].textofanswer);
+                            score.push(data[i].counter);
+                            color.push(getRandomColor());
+                        }
+
+                        var chartdata = {
+                            labels: player,
+                            datasets: [
+                                {
+                                    label: 'Player Score',
+                                    backgroundColor: color,
+                                    borderColor: 'rgba(200, 200, 200, 0.75)',
+                                    hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                                    hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                                    data: score
+                                }
+                            ]
+                        };
+
+                        var ctx = $("#mycanvas");
+
+                        var barGraph = new Chart(ctx, {
+                            type: 'pie',
+                            data: chartdata
+                        });
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    }
+                });
+            });
+
+        </script>
+
+
+
+
+    </body>
+</html>
--- res/lecdata.php
+++ res/lecdata.php
@@ -10,6 +10,17 @@ if (empty($temptab)) {
 //setting header to json
     header('Content-Type: application/json');
 
+
+    $tbl_quest = "question";
+    $tbl_ans = "answer";
+//print_r($temptab);
+
+    foreach ($temptab as $key => $value) {
+        $lid = $value;
+    }
+
+
+    //$stmt = $db->conn->prepare("SELECT textofanswer, counter FROM " . $tbl_ans . " WHERE qid = :quesID ORDER BY qid");
 //database
     define('DB_HOST', 'localhost');
     define('DB_USERNAME', 'root');
@@ -24,15 +35,48 @@ if (empty($temptab)) {
     }
 
 //query to get data from the table
-    $query = sprintf("SELECT playerid, score FROM score ORDER BY playerid");
+    $query = sprintf("SELECT textofanswer FROM " . $tbl_quest . " WHERE lid =" . $lid);
 
 //execute query
     $result = $mysqli->query($query);
 
 //loop through the returned data
-    $data = array();
+    /*$data = array();
     foreach ($result as $row) {
+
+        $query2 = sprintf("SELECT count FROM " . $tbl_ans . " WHERE lid =" . $lid . " AND textofanswer=" . $data['textofanswer']);
+        $result2 = $mysqli->query($query2);
+        $temp = 0;
+        $data2 = array();
+        foreach ($result2 as $row2) {
+            $data2[] = $row2;
+            $temp = $data2['count'];
+        }
+
+        $temp = $temp / sizeof($result);
+        print_r($temp);
         $data[] = $row;
+        print_r($data);
+    }*/
+    
+    foreach ($result as $key => $value) {
+
+        print_r($value['textofanswer']);
+        $query2 = sprintf("SELECT count FROM " . $tbl_ans . " WHERE lid =" . $lid . " AND textofanswer='" . $value['textofanswer']."'");
+        $result2 = $mysqli->query($query2);
+        $temp = 0;
+        $data2 = array();
+        
+        print_r($result2);
+        foreach ($result2 as $row2) {
+            $data2[] = $row2;
+            $temp = $data2['count'];
+        }
+
+        $temp = $temp / sizeof($result);
+        print_r($temp);
+        //$data[] = $row;
+       // print_r($data);
     }
 
 //free memory associated with result
@@ -43,5 +87,7 @@ if (empty($temptab)) {
 
     unset($_POST);
 //now print the data
-    print json_encode($data);
+    //bridge
+    //print json_encode($data);
+    //kaleis to consumer to js
 }
