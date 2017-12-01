c3058f63dfcdd4592218c55726b72756091e9b66
ssavva05 <ssavva05@ucy.ac.cy>
2017-12-01 15:51:18 +0200

    update enroll view

---
+++ lectures.php
@@ -0,0 +1,93 @@
+
+<?php require('includes/config.php'); 
+//define page title
+$title = 'My Lectures';
+
+//include header template
+require('layout/header.php'); 
+?>
+<?php //process addLecture form if submitted
+?>
+
+	<div class="row">
+
+	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+				<h2>My Lectures-<?php $cname=$_SESSION['cname']; $dateof=$_SESSION['dateof']; echo $cname." ";
+				$pieces = explode("-",$dateof); 
+					if ($pieces[1]=="09") 
+					 echo "Xειμερινό εξάμηνο ".$pieces[0];
+				 else if ($pieces[1]=="01") 
+				     echo "Eαρινό εξάμηνο ".$pieces[0]; ?></h2>
+				 <p><a href='memberpage.php'>Back to homepage</a></p>
+			<form action="" method="post">
+				<?php				
+				  $cname=$_SESSION['cname'];
+				  $stmt = $db->prepare('SELECT * FROM Lecture WHERE cname = :cname AND dateof =:dateof');
+			      $stmt->bindParam(':cname',$cname);
+				  $stmt->bindParam(':dateof',$dateof);
+			      $stmt->execute();
+				 while($result=$stmt->fetch(PDO::FETCH_ASSOC)){
+				 ?>
+				<input type="radio" name="radio" value="<?php echo $result["lid"];?>">
+				<?php echo "Lecture: ".$result["lname"]; ?>
+				</br>
+				<?php
+				}
+				?>
+				<?php
+				if(isset($_POST['editquestions']))
+					if(!isset($_POST['radio']))
+						echo '<p class="bg-danger">'."You have to select a lecture".'</p>';
+				
+			if(isset($_POST['editquestions'])){
+				 if(isset($_POST['radio'])){
+					  $_SESSION['lid']=$_POST['radio'];
+					  echo  "<script> window.location.href='myQuestions.php';</script>";
+
+				  }
+				}
+				if(isset($_POST['startlecture']))
+					if(!isset($_POST['radio']))
+						echo '<p class="bg-danger">'."You have to select a lecture".'</p>';
+				
+			if(isset($_POST['startlecture'])){
+				 if(isset($_POST['radio'])){
+					  $_SESSION['lid']=$_POST['radio'];
+					  echo  "<script> window.location.href='res/index.php';</script>";
+
+				  }
+				}
+				if(isset($_POST['addlecture'])){
+					  $_SESSION['cname']=$cname;
+					  $_SESSION['dateof']=$dateof;
+					  echo  "<script> window.location.href='LectureForm.php';</script>";
+				}
+				if(isset($_POST['deletion']))
+					if(!isset($_POST['radio']))
+						echo '<p class="bg-danger">'."You have to select a lecture".'</p>';
+				 
+				 
+		    if(isset($_POST['deletion'])){
+				 if(isset($_POST['radio'])){
+				  $lid=$_POST['radio'];
+				  $stmt = $db->prepare('DELETE FROM Lecture WHERE lid = :lid');
+			      $stmt->bindParam(':lid',$lid);
+			      $stmt->execute();
+				  echo  "<script> window.location.href='lectures.php';</script>";
+				 }
+				}
+				
+				?>
+				<input name="addlecture" type="submit" value="Add Lecture">
+				<input name="deletion" type="submit" value="Delete Lecture" >
+			    <input name="startlecture" type="submit" value="Start Lecture">
+			    <input name="editquestions" type="submit" value="Edit Questions">
+				
+		</form>
+	</div>
+</div>
+
+<?php 
+//include header template
+require('layout/footer.php'); 
+?>
